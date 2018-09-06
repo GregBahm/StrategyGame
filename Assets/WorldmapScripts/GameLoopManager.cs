6833a80165251ae299eadc6a9592c8c807473644
derp derp <workingbahm@yahoo.com.sg>
2018-09-06 09:14:05 -0700

    Stubbed out the DisplayTimings class

--- Assets/WorldmapScripts/GameLoopManager.cs
+++ Assets/WorldmapScripts/GameLoopManager.cs
@@ -44,11 +44,11 @@ public class GameDisplayManager
     
     public void DisplayTurn(GameTurnTransition turn, float progression)
     {
-
+        DisplayTimings timings = new DisplayTimings(progression);
         // First new units are generated
 
         // Then armies move
-        UpdateArmies(turn.ArmyTransitions, progression);
+        UpdateArmies(turn.ArmyTransitions, timings);
 
         // Then display upgrades
         // Then display mergers
@@ -56,7 +56,7 @@ public class GameDisplayManager
         // Then move units towards rally points
     }
 
-    private void UpdateArmies(IEnumerable<ArmyTurnTransition> armyTransitions, float progression)
+    private void UpdateArmies(IEnumerable<ArmyTurnTransition> armyTransitions, DisplayTimings progression)
     {
         Dictionary<Guid, ArmyTurnTransition> transitions = 
             armyTransitions.ToDictionary(transition => transition.StartingState.Identifier, transition => transition);
@@ -106,10 +106,35 @@ public class ArmyDisplay
         ArtContent.SetActive(false);
     }
 
-    internal void DisplayArmy(ArmyTurnTransition transition, float progression)
+    internal void DisplayArmy(ArmyTurnTransition transition, DisplayTimings progression)
     {
         ArtContent.SetActive(true);
+        
+    }
+}
 
+public struct DisplayTimings
+{
+    public float NewUnits { get; }
+    public float ProvinceEffects { get; }
+    public float RoutingArmyRecovery { get; }
+
+    public float ArmiesMoveToCollision { get; }
+    public float ArmiesDieFromCollisionBattles { get; }
+    public float ArmiesToDestination { get; }
+    public float ArmiesDieFromNonCollisionBattles { get; }
+
+    public float ProvinceOwnershipChanges { get; }
+    public float ProvinceMergers { get; }
+    public float ProvinceUpgrades { get; }
+
+    public float RallyChanges { get; }
+    public float NewArmyCreated { get; }
+    public float UnitsMove { get; }
+    public float PlayersDead { get; }
+
+    public DisplayTimings(float turnProgression)
+    {
 
     }
 }
