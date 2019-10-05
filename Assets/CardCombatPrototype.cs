fd825cd142c7737361d7d440a1c0dd91f3e4e976
derp derp <workingbahm@yahoo.com.sg>
2019-10-05 08:58:02 -0700

    Starting card prototype

---
+++ Assets/CardCombatPrototype.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+public class CardCombatPrototype : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class CardBattleState
+{
+    public ReadOnlyCollection<Card> PriorCards { get; }
+    public Card CurrentCard { get; }
+    public ReadOnlyCollection<Card> RemainingCards { get; }
+}
+
+public class CardDescription
+{
+    public string Name { get; }
+}
+
+public class Card
+{
+    public CardFaction Faction { get; }
+    public CardDescription Description { get; }
+}
+
+public enum CardBattleStateOutcome
+{
+    SideAWon,
+    SideBWon,
+    Tie,
+    Undecided
+}
+
+public enum CardFaction
+{
+    SideA,
+    SideB,
+    Neutral
+}
+
+public enum Rank
+{
+    Front,
+    Mid,
+    Back
+}
+
+public class TargetableCard : Card
+{
+    public int MaxHitpoints { get; }
+    public int CurrentHitpoints { get; }
+
+    public int Attack { get; }
+    public int Defense { get; }
+    
+    public int MaxMoral { get; }
+    public int RemainingMoral { get; }
+
+    public Rank Rank { get; }
+}
