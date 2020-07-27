938695b2e73807f3726472443c1ddd4cf0092a64
derp derp <workingbahm@yahoo.com.sg>
2020-07-26 17:30:29 -0700

    Just messing with visuals

--- StolenThronesCombatPrototype/Assets/GlobalShaderManager.cs
+++ StolenThronesCombatPrototype/Assets/GlobalShaderManager.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.WSA;
 
 public class GlobalShaderManager : MonoBehaviour
 {
+
     void Update()
     {
         Shader.SetGlobalMatrix("_DisplayVolume", transform.worldToLocalMatrix);
---
+++ StolenThronesCombatPrototype/Assets/RemoteConnector.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.WSA;
+
+public class RemoteConnector : MonoBehaviour
+{
+    private string IP = "10.0.0.164";
+
+    private bool connected;
+
+    public void Connect()
+    {
+        if (HolographicRemoting.ConnectionState != HolographicStreamerConnectionState.Connected)
+        {
+            HolographicRemoting.Connect(IP, 99999, RemoteDeviceVersion.V2);
+        }
+    }
+
+    private void Start()
+    {
+        Connect();
+    }
+
+    void Update()
+    {
+        Shader.SetGlobalMatrix("_DisplayVolume", transform.worldToLocalMatrix);
+
+        if (!connected && HolographicRemoting.ConnectionState == HolographicStreamerConnectionState.Connected)
+        {
+            connected = true;
+
+            StartCoroutine(LoadDevice("WindowsMR"));
+        }
+    }
+
+    IEnumerator LoadDevice(string newDevice)
+    {
+        XRSettings.LoadDeviceByName(newDevice);
+        yield return null;
+        XRSettings.enabled = true;
+    }
+}
